4058f22aba263b85fd6b2c6ef050f05a7be88254
Martin Ying <martinying@yahoo.com>
2018-04-07 16:12:49 -0700

    Changed algorithm, starting with how elevators are found to service up/down button pressed.  Instead of using the elevator with the smallest queue to service request.  Looked for closest idle elevator.  If none exists then queue up requests.  This introduces a queue concept.  Elevators' chance of being idles now is increased therefore needed to add to the idle event to look in queue to do the next thing.
    Consolidated up/down button event handling into a common function.
    
    Removed the use of underscore library in favor of pure javascript.

--- elevator.js
+++ elevator.js
@@ -1,60 +1,69 @@
 {
-	init: function(elevators, floors) {
-		var findElevator = function(elevators) {
-			var result;
-			var minDesintaitonQueueLength = Number.MAX_SAFE_INTEGER;
-			_.each(elevators, function(elevator) {
-				if (elevator.destinationQueue.length < minDesintaitonQueueLength) {
-					result = elevator;
-					minDesintaitonQueueLength = elevator.destinationQueue.length;
-				}
-			});
-			return result;
-		}
+    init: function(elevators, floors) {
+        var queue = new Array();
 
-		var elevatorIsGoingToFloorAlready = function(elevator, floorNum) {
-			var q = elevator.destinationQueue;
-			return _.contains(q, floorNum);
-		}
+        //find closest idle elevator
+        var findElevator = function(floorNum) {
+            var result;
+            var minDistance = Number.MAX_SAFE_INTEGER;
+            for(var index in elevators) {
+                var distance = Math.abs(elevators[index].currentFloor()-floorNum)
+                if (elevators[index].destinationQueue.length === 0 && distance < minDistance) {
+                    minDistance = distance;
+                    result = elevators[index];
 
-		//initialize elevators
-		_.each(elevators, function(elevator) {
-			elevator.on("floor_button_pressed", function(floorNum) {
-				if (!elevatorIsGoingToFloorAlready(this, floorNum)) {
-					this.goToFloor(floorNum);
-				}
-			});
-		});
+                }
+            }
+            return result;
+        }
 
+        //initialize elevators
+        elevators.forEach(function(elevator) {
+            elevator.on("floor_button_pressed", function(floorNum) {
+                if (this.destinationQueue.indexOf(floorNum) < 0) {
+                    this.goToFloor(floorNum);
+                }
+            });
 
-		//intialzie floors
-		_.each(floors, function(floor) {
-			floor.on("up_button_pressed", function(e) {
-				return function(floor) {
-					var elevator = findElevator(e);
-					var q = elevator.destinationQueue;
-					if (!elevatorIsGoingToFloorAlready(elevator, floor.floorNum())) {
-						q.push(floor.floorNum());
-						elevator.destinationQueue = q;
-						elevator.checkDestinationQueue();
-					}
-				};
-			}(elevators));
+            elevator.on("idle", function() {
+                if(queue.length > 0) {
+                    //the elevator is idle , destination queue will always be empty so no need to check if elevator will be going there
+                    //provided we ensure there are no dups in the queue, see floor behavior                    
+                    this.goToFloor(queue.shift());
+                }
+            });
 
-			floor.on("down_button_pressed", function(e) {
-				return function(floor) {
-					var elevator = findElevator(e);
-					var q = elevator.destinationQueue;
-					if (!elevatorIsGoingToFloorAlready(elevator, floor.floorNum())) {
-						q.push(floor.floorNum());
-						elevator.destinationQueue = q;
-						elevator.checkDestinationQueue();
-					}
-				};
-			}(elevators));
-		});
-	},
-	update: function(dt, elevators, floors) {
-		// We normally don't need to do anything here
-	}
+            elevator.on("stopped_at_floor", function(floorNum) {
+                var i = queue.indexOf(floorNum);
+                if (i >= 0) {
+                    queue.splice(i, 1);
+                }
+            });
+        });
+
+        var floorBehavior = function() {
+            //CAN'T PUT INTO QUEUE IMMEDIATELY BECAUSE FIRST MOVE IS USUALLIY THIS AND THE ELEVATOR HAS NOT CALLED IDLE YET
+            //assumes floor context            
+            var elevator = findElevator(this.floorNum());
+            if (elevator) {
+                //it's an idle elevator so should not need to check if going to floor already                
+                elevator.goToFloor(this.floorNum());
+            } else {
+                //THIS CHECK IS THE BOUNDARY CONDITION IF ONE DIRECIOTN IS PRESSED ALREADY AND THEN THE OTHER DIRECITON IS PRESSED
+                //NO NEED TO GO TO FLOOR TWICE.  THIS CHECK MIGHT BE ABLE TO BE REMOVED WHEN WE ADD INDICATOR AND SEPERATE OUT UP/DOWN                 \
+                if(queue.indexOf(this.floorNum()) === -1) {
+                    queue.push(this.floorNum());
+                }
+            }
+        }
+
+        //intialzie floors
+        floors.forEach(function(floor) {
+            floor.on("up_button_pressed", floorBehavior);
+            floor.on("down_button_pressed", floorBehavior);
+        });
+    },
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
